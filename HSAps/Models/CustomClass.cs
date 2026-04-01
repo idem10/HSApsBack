caa2ea2c4f0bd5ea4e60807738767dcd6a890719
hector salazar <hector_daniel.h.salazar@daimlertruck.com>
2026-04-01 13:36:31 -0600

    add - cors

---
+++ HSAps/Models/CustomClass.cs
@@ -0,0 +1,12 @@
+﻿namespace HSAps.Models
+{
+    public class DashBoardDefinition
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Version { get; set; }
+        public string Description { get; set; }
+        public string ContactName { get; set; }
+        public string UrlContactName { get; set; }
+    }
+}
--- HSAps/Program.cs
+++ HSAps/Program.cs
@@ -1,24 +1,78 @@
+using HSAps.Models;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+List<DashBoardDefinition> ddf = builder.Configuration.GetSection("DashBoard").Get<List<DashBoardDefinition>>();
+IConfiguration configuration = builder.Configuration;
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+builder.Services.AddCors(op => op.AddPolicy("AllowAll", policy =>
+{
+    policy.AllowAnyMethod()
+        .AllowAnyHeader()
+        .SetIsOriginAllowed(origin => true)
+        .AllowCredentials();
+}));
 
+builder.Services.AddSwaggerGen(c =>
+{
+    foreach (var conf in ddf)
+    {
+        c.SwaggerDoc(conf.Name,
+            new OpenApiInfo
+            {
+                Title = conf.Title,
+                Version = conf.Version,
+                Description = conf.Description,
+                Contact = new OpenApiContact
+                {
+                    Name = conf.ContactName,
+                    Url = new Uri(conf.UrlContactName)
+                }
+            });
+    }
+});
+builder.Services.AddControllersWithViews().
+                    AddJsonOptions(options =>
+                    {
+                        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+                        options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                    });
 var app = builder.Build();
-
+app.UseCors("AllowAll");
+string baseFolder = "..";
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    baseFolder = "";
 }
+app.UseSwagger();
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
+app.UseAuthentication();
 app.UseAuthorization();
+app.UseSwaggerUI(c =>
+{
+    foreach (var conf in ddf)
+    {
+        c.SwaggerEndpoint($"{baseFolder}/swagger/" + conf.Version + "/swagger.json", conf.Version);
+    }
+});
+
+//sockets
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.MapControllers();
 
