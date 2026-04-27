e148cd4ba42247bb764bcdcf369aa3650620e4fe
hector salazar <hector_daniel.h.salazar@daimlertruck.com>
2026-04-27 02:30:05 -0600

    agregar foto de perfil

---
+++ HSAps/Interfaces/Class.cs
@@ -0,0 +1,8 @@
+﻿        namespace HSAps.Models
+    {
+        public class UpdateImageClass
+        {
+            public string UserName { get; set; }
+            public string Imagen { get; set; }
+        }
+    }
